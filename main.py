f37b75e8552480e3d58aff7f05cbb68223a30f7a
Yuliya <fokju@yandex.ru>
2022-10-14 12:26:31 +0300

    +импорт, экспорт

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ast import parse
+from turtle import down
 import telebot
 from telebot import types
 import random
@@ -9,12 +10,6 @@ bot = telebot.TeleBot(token)
 userkal=[]
 #@bot.message_handler(content_types=['text'])#commands=['start'])
 
-def main_menu():
-    print('\Выберите:')
-    print('1. Показать все контакты') #Таблицей
-    print('2. Добавить контакты')
-    x=1
-    return x
     
 
 @bot.message_handler(content_types=['text'])
@@ -25,7 +20,9 @@ def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     but1 = types.KeyboardButton("Показать все контакты")
     but2 = types.KeyboardButton("Добавить контакт")
-    markup.add(but1, but2)
+    but3 = types.KeyboardButton("Импорт контактов")
+    but4 = types.KeyboardButton("Экспорт контактов")
+    markup.add(but1, but2,but3,but4)
 
     #bot.reply_to(message, "Здравствуй, {0.first_name}\nСмотрю, ты за Единую, Великую и Недилимую".format(message.from_user)
   #,parse_mode='html',reply_markup=markup)
@@ -45,7 +42,47 @@ def start(message):
         #bot.reply_to(message,"Уточните Имя")
         #name=message.text
         #bot.reply_to(message,fam +""+ name +"Добавлен")
+    if message.text=="Экспорт контактов":
+        exportcontacts(message)
+    if message.text=="Импорт контактов":
+        importcontacts(message)
 
+def importcontacts(message):
+    bot.send_message(message.chat.id,"Жду файл!")
+    #bot.register_next_step_handler(message,save)
+    bot.register_next_step_handler(message,importcontacts2)
+
+def importcontacts2(message):
+    file_name = message.document.file_name
+    file_id = message.document.file_name
+    file_id_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_id_info.file_path)
+    src = file_name
+    with open( src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+    with open(src,encoding = 'utf-8') as csvfile:
+            spamreader = csv.reader(csvfile ,delimiter=';') #, quotechar='|')
+            for row in spamreader:
+                zapiska = []
+                with open (r'bookT.csv',"a", newline='',encoding = 'utf-8') as writerowtobook:
+                    writer=csv.writer(writerowtobook,delimiter=';', quoting=csv.QUOTE_MINIMAL)
+                    writer.writerow(row)
+    #newFile = message.effective_attachment.get_file()
+    #newFile.download('file_name')
+    #with open(path,'wb') as new_file:
+    #new_file.write(downloaded_file)
+
+    bot.send_message(message.chat.id,src +" импортирован")
+    bot.clear_step_handler_by_chat_id(message.chat.id)
+    start(message)
+    #file_id = message.voice.file_id
+    #newFile = bot.get_file(file_id)
+    #newFile.download('voice.ogg')
+
+def exportcontacts(message):
+    bot.send_message(message.chat.id,"Контакты выгружены")
+    bot.send_document(message.chat.id, document=open('bookT.csv',encoding = 'utf-8'))
+    
 def fam(message):
     chat_id=message.chat.id   
     fam=message.text
